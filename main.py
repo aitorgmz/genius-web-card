e46149a70d3353e2903f8dd585c84584179bfad4
aitorgmz <70070555+aitorgmz@users.noreply.github.com>
2023-05-06 14:44:44 +0200

    Refactorized code in order to use Pillow
    We refactorized the script in order to use Pillow, which allows us to use custom fonts.

--- main.py
+++ main.py
@@ -4,15 +4,22 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import cv2
+import numpy as np
+from PIL import ImageFont, ImageDraw, Image
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 font = cv2.FONT_HERSHEY_SIMPLEX
 textThickness = 2
 fontScale = 1
+pillowFont = ImageFont.truetype("C:\\Users\\Aitor\\Desktop\\Programme.ttf", 50)
 def createImage(pathToFile, songText, songAutor, songName):
     print("Iniciando creacion imagen")
     img = cv2.imread(pathToFile)
+
+    #conversion to pillow(needed to write with custom font)
+    cv2ImgArray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
     width, height, channels = img.shape
     print("Image data: ", height, width, channels)
 
@@ -20,16 +27,29 @@ def createImage(pathToFile, songText, songAutor, songName):
     lyricPosY = (height-200)
     lyricPos = (lyricPosX, lyricPosY)
 
-    writeTextWithBackground(img, songText, black, white, lyricPosX, lyricPosY)
-    cv2.putText(img, songAutor + " \"" + songName + "\"", (lyricPosX, lyricPosY+50), font, fontScale, white, textThickness, 2)
+    cv2ImgArray = writeTextWithBackgroundPillow(cv2ImgArray, songText, white, white, lyricPosX, lyricPosY)
+    cv2ImgArray = writeTextWithoutBackground(cv2ImgArray,  songAutor + " \"" + songName + "\"", lyricPosX, lyricPosY+75)
 
-    cv2.imwrite("C:\\Users\\Aitor\\Desktop\\final.jpg", img)
+    cv2.imwrite("C:\\Users\\Aitor\\Desktop\\final.jpg", cv2ImgArray)
     cv2.waitKey(0)
 
-def writeTextWithBackground(img, text, textColor, bgColor, posX, posY):
+
+def writeTextWithBackgroundPillow(cv2ImageArray,text, textColor, bgColor, posX, posY):
     textUbication, textHeight = cv2.getTextSize(text, font, fontScale, textThickness)
-    cv2.rectangle(img, (posX, posY+10), (posX+textUbication[0], posY-textUbication[1]), bgColor, -1)
-    cv2.putText(img, text, (posX, posY), font, fontScale, textColor, textThickness, 2)
+    pillowImage = Image.fromarray(cv2ImageArray)
+    pillowDrawType = ImageDraw.Draw(pillowImage)
+    pillowDrawType.rectangle([(posX, posY),(posX+925, posY+50)], fill="#FFF")
+    pillowDrawType.text((posX, posY), text, font=pillowFont, fill="#000")
+    cv2ImageArray = cv2.cvtColor(np.array(pillowImage), cv2.COLOR_RGB2BGR)
+    return cv2ImageArray
+
+def writeTextWithoutBackground(cv2ImageArray, text, posX, posY):
+    pillowImage = Image.fromarray(cv2ImageArray)
+    pillowDrawType = ImageDraw.Draw(pillowImage)
+    pillowDrawType.text((posX, posY), text, font=pillowFont, fill="#FFF")
+    cv2ImageArray = cv2.cvtColor(np.array(pillowImage), cv2.COLOR_RGB2BGR)
+    return cv2ImageArray
+
 
 
 # Press the green button in the gutter to run the script.
