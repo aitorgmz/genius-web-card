db783eb018d693024779221788daa28cf1bd1c1c
aitorgmz <70070555+aitorgmz@users.noreply.github.com>
2023-10-10 14:08:58 +0200

    Improved image creation
    Improved the image creation algorithm so it can handle multiple lines of text better.

--- web/geniuscards/modules/geniuscards.py
+++ web/geniuscards/modules/geniuscards.py
@@ -20,24 +20,56 @@ def createImage(base_64, song_text, song_autor, song_name, is_local_file):
 
     width, height, channels = img.shape
 
-    pillow_font_programme = ImageFont.truetype("C:\\Users\\Aitor\\Desktop\\Programme.ttf", int(0.048*width))
+    pillow_font_programme = ImageFont.truetype("C:\\Users\\Aitor\\Desktop\\Programme.ttf", int(0.043*width))
     pillow_font_georgia = ImageFont.truetype("C:\\Users\\Aitor\\Desktop\\Georgia.ttf", int(0.1*width))
 
     #default values for both left and top margin
     margin_x = 0.048*width
-    line_height = width*0.065
+    line_height = width*0.063
 
-    song_text_length = len(song_text)
-    if song_text_length < 120:
-        pos_y = (height-(height*0.30))
+    song_text_lines_array = song_text.splitlines()
+    song_text_array = []
+    line_length = 0
+    line_text = ""
+    for line in song_text_lines_array:
+        if len(line) > 40:
+            line_words = line.split(" ")
+            for word in line_words:
+                if line_length + len(word) < 36:
+                    if line_text != "" :
+                        line_text = line_text + " " + word
+                    else:
+                        line_text = word
+                    line_length += len(word)
+                else:
+                    line_text = line_text + " "
+                    song_text_array.append(line_text)
+                    line_text = word
+                    line_length = len(word)
+            line_text = line_text + " "
+            song_text_array.append(line_text)
+            line_text = ""
+            line_length = 0
+        else:
+            song_text_array.append(line)
+
+    if len(song_text_array) <= 2:
+        pos_y = (height - (height * 0.26))
+    elif len(song_text_array) <= 4:
+        pos_y = (height-(height*0.36))
+    elif len(song_text_array) <= 6:
+        pos_y = (height-(height*0.42))
+    elif len(song_text_array) <= 8:
+        pos_y = (height-(height*0.60))
+    elif len(song_text_array) <= 10:
+        pos_y = (height-(height*0.70))
     else:
-        pos_y = (height-(height*0.45))
+        pos_y = (height-(height*0.85))
 
-    song_text_array = [song_text[i:i+40] for i in range(0, song_text_length, 40)]
     for i,line in enumerate(song_text_array):
         cv2_img_array = write_text_with_background(cv2_img_array, line, "#000", "#FFF", pillow_font_programme,
                                                  2*margin_x, pos_y + (line_height*line_count))
-    cv2_img_array = write_text_no_background(cv2_img_array,  song_autor + " \"" + song_name + "\"", pillow_font_programme,
+    cv2_img_array = write_text_no_background(cv2_img_array,  song_autor.lstrip() + " \"" + song_name + "\"", pillow_font_programme,
                                              2*margin_x, pos_y+(line_height*line_count))
     cv2_img_array = write_text_no_background(cv2_img_array, "“", pillow_font_georgia,
                                              margin_x, pos_y - (width*0.03))
@@ -54,7 +86,7 @@ def write_text_with_background(cv2_image_array,text, text_color, bg_color, font,
     pillow_image = Image.fromarray(cv2_image_array)
     pillow_draw_type = ImageDraw.Draw(pillow_image)
     text_pos_x, text_pos_y = get_text_dimensions(text, font)
-    pillow_draw_type.rectangle([(pos_x, pos_y),(pos_x + text_pos_x, pos_y + text_pos_y)],
+    pillow_draw_type.rectangle([(pos_x - 5, pos_y),(pos_x + text_pos_x + 8, pos_y + text_pos_y)],
                                fill=bg_color)
     pillow_draw_type.text((pos_x, pos_y), text, font=font, fill=text_color)
     cv2_image_array = cv2.cvtColor(np.array(pillow_image), cv2.COLOR_RGB2BGR)
