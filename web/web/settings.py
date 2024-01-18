054f68f4646652a62262db27762e7f435cbe3216
aitorgmz <70070555+aitorgmz@users.noreply.github.com>
2024-01-18 17:32:01 +0100

    Added basic internationalization
    Added basic internationalization settings using django´s i18n

--- web/web/settings.py
+++ web/web/settings.py
@@ -106,13 +106,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = (os.path.join(BASE_DIR, os.path.join('mainpage', 'locale')),)
 
 
 # Static files (CSS, JavaScript, Images)
