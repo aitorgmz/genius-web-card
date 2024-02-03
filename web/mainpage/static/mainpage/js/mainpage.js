e624ff963d2d87798c156748e5fab341df6768f4
aitorgmz <70070555+aitorgmz@users.noreply.github.com>
2024-02-03 12:48:39 +0100

    Added main tabs
    Added main tabs, navigation between them and a little content.

--- web/mainpage/static/mainpage/js/mainpage.js
+++ web/mainpage/static/mainpage/js/mainpage.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
-
+    initializeBtns();
 });
+
+function initializeBtns(){
+
+    $("#contactBtn").click(function(){
+        $("#contactTab").css("display","flex");
+        $("#projectsTab").css("display","none");
+        $("#aboutMeTab").css("display","none");
+    });
+
+    $("#aboutMeBtn").click(function(){
+        $("#aboutMeTab").css("display","flex");
+        $("#contactTab").css("display","none");
+        $("#projectsTab").css("display","none");
+    });
+
+     $("#projectsBtn").click(function(){
+        $("#contactTab").css("display","none");
+        $("#projectsTab").css("display","flex");
+        $("#aboutMeTab").css("display","none");
+    });
+
+    $("#linkedInBtn").click(function(){
+        window.open("https://es.linkedin.com/in/aitor-g%C3%B3mez-taboada-821330217", "_blank");
+    });
+
+    $("#mailToBtn").click(function(){
+        window.open("mailto:aitorgtaboada@gmail.com", "_blank");
+    });
+}
 
